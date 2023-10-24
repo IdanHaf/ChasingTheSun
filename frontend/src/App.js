b409cdda924329b3799b920f88d86f5bde814935
OriBenjamin <102872346+OriBenjamin@users.noreply.github.com>
2023-10-24 22:46:11 +0300

    added inteface for Map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,84 +1,49 @@
+import React from 'react';
+import usePanorama from './Map';
 
-import React, { useState } from 'react';
-import './App.css';
-import Counter from './Counter';
-// import { withScriptjs, withGoogleMap, StreetViewPanorama, GoogleMap } from "react-google-maps";
-import { GoogleMap, StreetViewPanorama } from "react-google-maps";
-import withScriptjs from 'react-google-maps/lib/withScriptjs';
-import withGoogleMap from 'react-google-maps/lib/withGoogleMap';
-import {useLoadScript} from '@react-google-maps/api';
-import { LoadScript } from "@react-google-maps/api";
-const MyMap = withScriptjs(withGoogleMap((props) => {
-  <GoogleMap/>;
-//   // Define the container style for the Google Map
-//   mapContainerStyle={{
-//     width: '100%',
-//     height: '400px',
-//   }}
-//   // center={center} // Set the center of the map
-//   zoom={10} // Set the initial zoom level
-// >
-//   <StreetViewPanorama
-//     id="street-view"
-//     // Define the container style for the Street View Panorama
-//     mapContainerStyle={{
-//       width: '100%',
-//       height: '400px', // Adjust this to your desired height
-//     }}
-//     // position={center} // Set the position of the Street View panorama
-//     visible={true} // Make the Street View panorama visible
-//   />
-// </GoogleMap>
-}
-));
-
-const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh',
-}
-const center = {
-  lat: 31.968599,
-  lng: -99.901810,
-}
-const lib = ["places"];
-const key = "AIzaSyD4J0LPRji3WKllVxLji7YDbd5LSt6HA7o"; // PUT GMAP API KEY HERE
 function App() {
-    const{isLoaded, loadError} = useLoadScript({
-      // Uncomment the line below and add your API key
-      googleMapsApiKey: 'AIzaSyD4J0LPRji3WKllVxLji7YDbd5LSt6HA7o',
-  });
-
-if (loadError) return "Error loading Maps";
-if (!isLoaded) return "Loading Maps";
-    return (
-        <div className="container mx-auto mt-10">
-            <h1 className="text-2xl font-semibold mb-4 text-center">React Basics</h1>
-
-            <div className="absolute w-3/4 h-3/4">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!4v1697820801337!6m8!1m7!1se2U9Q_eyUlOIqrt9byrCfA!2m2!1d40.75403943954401!2d-73.99197153053622!3f324.1710339446496!4f21.36979247343878!5f1.22634496099663"
-                    title="Embedded Content from Example.com" width="100%" height="100%" loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade">
-                </iframe>
-            </div>
-
-            <Counter/>
-            
-            {/* <MyMap
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD4J0LPRji3WKllVxLji7YDbd5LSt6HA7o"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `400px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            /> */}
-            {/* <LoadScript googleMapsApiKey={key} libraries={lib}>
-      <GoogleMap 
-        mapContainerStyle={mapContainerStyle} 
-        zoom={11} 
-        center={center} 
-        />
-        </LoadScript> */}
+    const [panoRef, panoramaState, setPov] = usePanorama();
+    // console.log(panoramaState.pov)
+    // setPanoramaState((prevPano) => { return {...prevPano, id: "ChIJN1t_tDeuEmsRUsoyG83frY4"}});
+  return (
+    <div className="container mx-auto mt-10">
+      <h1 className="text-2xl font-semibold mb-4 text-center">React Basics</h1>
+        <div id="pano"></div>
+        <button className="w-5 h-5 bg-black" onClick={()=>{
+            setPov( (oldH, oldpP)=>{return {heading: oldH+1, pitch: oldpP+1}} )
+        }}></button>
+        <div id="floating-panel">
+            <table>
+                <tbody>
+                <tr>
+                    <td><b>Position</b></td>
+                    <td id="position-cell">{JSON.stringify(panoramaState?.position)}</td>
+                </tr>
+                <tr>
+                    <td><b>POV Heading</b></td>
+                    <td id="heading-cell">{panoramaState.pov?.heading}</td>
+                </tr>
+                <tr>
+                    <td><b>POV Pitch</b></td>
+                    <td id="pitch-cell">{panoramaState.pov?.pitch}</td>
+                </tr>
+                <tr>
+                    <td><b>Pano ID</b></td>
+                    <td id="pano-cell">{panoramaState.id}</td>
+                </tr>
+                <tr>
+                    <td><b>Zoom</b></td>
+                    <td id="zoom-cell">{panoramaState.zoom}</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
-    );
+        {/* <Streetview apiKey="AIzaSyD4J0LPRji3WKllVxLji7YDbd5LSt6HA7o"></Streetview> */}
+        
+        <div ref={panoRef} if="pano" style={{ height: '400px' }}></div>
+        {/* <Map /> */}
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/Map.js
@@ -0,0 +1,116 @@
+import React, { useEffect } from 'react';
+import {useRef, useState} from 'react';
+import { Loader } from "@googlemaps/js-api-loader"
+
+function usePanorama() {
+    const [panoramaState, setPanoramaState] = useState({
+        id: null,
+        position: null,
+        pov: null,
+        zoom: null,
+    }); 
+
+    const panoRef = useRef(null);
+    const API_KEY = "AIzaSyD4J0LPRji3WKllVxLji7YDbd5LSt6HA7o";
+
+    
+    const [loaded, setLoaded] = useState(false);
+    const [setPanorama, setPanoramaLoader] = useState(null);
+    let panorama = null;
+
+    // useEffect(() => {
+    //     // set panoramas properties
+    //     if (!panorama) return;
+    //     panorama.setPano(panoramaState.id);
+    //     panorama.setPosition(panoramaState.position);
+    //     panorama.setPov(panoramaState.pov);
+    //     panorama.setZoom(panoramaState.zoom);
+    // }
+    // , [panoramaState]);
+    useEffect(() => {
+        if (loaded) return;
+            setLoaded(true);
+        const loader = new Loader({
+            apiKey: API_KEY,
+            version: "weekly",
+          });
+    
+        
+        loader.importLibrary("streetView").then(async (streetViewLibrary) => {
+            
+            if (streetViewLibrary && panoRef.current) {
+                
+                const {StreetViewPanorama} = streetViewLibrary;
+                console.log("everything's loaded, let's go");
+                const panorama = new StreetViewPanorama(panoRef.current, {
+                    position: { lat: 37.869, lng: -122.255 },
+                    pov: {
+                        heading: 270,
+                        pitch: 0,
+                    },
+                    visible: true,
+                    fullscreenControl: false,
+                    disableDefaultUI: true,
+                    motionTracking: false,
+                    motionTrackingControl: false,
+                });
+                
+                panorama.addListener("pano_changed", () => {
+                    setPanoramaState((prevPano) => { return {...prevPano, id: panorama.getPano()}});
+                });
+                panorama.addListener("position_changed", () => {
+                    setPanoramaState((prevPano) => { return {...prevPano, position: panorama.getPosition()}});
+                });
+                panorama.addListener("pov_changed", () => {
+                    setPanoramaState((prevPano) => { return {...prevPano, pov: panorama.getPov()}});
+                });
+                panorama.addListener("zoom_changed", () => {
+                    setPanoramaState((prevPano) => { return {...prevPano, zoom: panorama.getZoom()}});
+                });
+                
+                setPanoramaLoader((p) => 
+                {
+                    return (newPovFunc) => {
+                    
+                    setPanoramaState((prevPano) => { 
+                        const newPov = newPovFunc(prevPano.pov.heading, prevPano.pov.pitch);
+                        panorama.setPov(newPov);
+                        return {...prevPano, pov: newPov};
+                    } );
+                    }
+                }
+                )
+                // setPanoramaLoader((p) => 1);
+    
+               
+    
+            } else {
+                console.error('Google Maps API or StreetViewPanorama not loaded');
+            }
+        });
+        
+        return () => {
+            if (panorama) {
+              panorama.removeListener('pano_changed');
+              panorama.removeListener('position_changed');
+              panorama.removeListener('pov_changed');
+              panorama.removeListener('zoom_changed');
+              panorama.setVisible(false); // Hide the panorama
+              panorama = null; // Remove the reference
+            }
+          };
+    
+    }, []);
+    
+ 
+function Map() {
+
+
+    return (
+     <div ref={panoRef} if="pano" style={{ height: '400px' }}></div>
+    );
+}
+return [panoRef, panoramaState, setPanorama];
+}
+
+export default usePanorama;

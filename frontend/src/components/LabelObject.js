cfd1d73bc8be43fea91da259b49e601fced42fe4
IdanHaf <idan.hafner@campus.technion.ac.il>
2023-10-26 18:09:00 +0300

    Added start of label button

--- frontend/src/components/LabelObject.js
+++ frontend/src/components/LabelObject.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import usePanorama from '../usePanorama';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/LabelObject.css';
 
 function LabelObject() {
     const [buttonPressed, setButtonPressed] = useState(false);
     const [showDiv, setShowDiv] = useState(false);
     const [[xPosition,yPosition], setPosition] = useState([0, 0]);
+    const [[xWidth,yHeight], setWidth] = useState([0, 0]);
+
 
     const handleButtonClick = () => setButtonPressed(true);
     const handlePageClick = (e) => {
@@ -15,6 +16,26 @@ function LabelObject() {
             setPosition([e.clientX, e.clientY]);
         }
     };
+
+    const handleMouseMove = (e) =>{
+        setWidth([(e.clientX - xPosition), (e.clientY - yPosition)]);
+    };
+
+    const handleMouseUp = () =>{
+        window.removeEventListener('mousemove', handleMouseMove);
+    };
+
+    useEffect(() => {
+        if(showDiv){
+            window.addEventListener('mousemove', handleMouseMove);
+            window.addEventListener('mouseup', handleMouseUp);
+        }
+
+        //Cleanup.
+        return () => {
+            window.removeEventListener('mousemove', handleMouseUp);
+        }
+    }, [showDiv])
 
 
     return (
@@ -29,7 +50,8 @@ function LabelObject() {
             {
                 showDiv &&
                 <div className= "labelDiv"
-                     style={{position: 'absolute', top: yPosition + 'px', left: xPosition + 'px'}}>
+                     style={{position: 'absolute', top: yPosition + 'px', left: xPosition + 'px', width: xWidth,
+                             height: yHeight}}>
                 </div>
             }
         </>

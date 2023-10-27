7a29997c34a27d1f500380613d163112cbbccf58
OriBenjamin <102872346+OriBenjamin@users.noreply.github.com>
2023-10-27 23:12:33 +0300

    some start for the intelligence clues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Panorama from './pages/Panorama';
 import KeyMap from './pages/KeyMap';
+import Clues from './components/Clues';
 import IntelligenceMode from './pages/IntelligenceMode';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link } from "react-router-dom";
@@ -16,7 +17,7 @@ function App() {
             <Route path="/" element={<Panorama/>}/>
             <Route path="/keymap" element={<KeyMap/>}/>
             <Route path="/intelligenceMode" element={<IntelligenceMode/>}/>
-
+            <Route path="/clues" element={<Clues/>}/>
           </Routes>
         </Router>
   );

7e5162c8f631c4dbd1ad4b24ad5b861b879e12ea
IdanHaf <idan.hafner@campus.technion.ac.il>
2023-11-03 23:14:59 +0200

    improved LabelSelector

--- frontend/src/components/LabelSelector.js
+++ frontend/src/components/LabelSelector.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/LabelSelector.css'
-import zoomToPitch from '../data/ZoomToPitch.json'
+import zoomToRatioData from '../data/RatioData.json'
+import { objectPositionOnScreen, closest } from "../helpFunctions/LabelSelectorHelpers";
 
 /*
     An upgraded LabelObject component, with darkening animation and a resizable label.
@@ -54,109 +55,46 @@ function LabelSelector(props) {
     const panoramaState = props.panoramaState;
     const [[xTrack, yTrack], setTrack] = useState([0,0]);
 
-    const hasDetected = () => {
-
-    }
-
     /*
-        TODO:: calculate the max pitch and end pitch using the ratio of pitch to height.
-        TODO:: move inside of useEffect, with zoom and lat & lng dependencies.
-        For later - console.log(panoramaState?.position.lat() === 37.868996731655);
-
-        The function receives the position of the bottom-right side of the label.
-        Calculating the objects location based on pitch and heading.
-
-        returns true if the object was found, false otherwise.
+        TODO:: check if in the right lat&lon.
+        The function receives event of mouseUp.
+        Returns true if object was labeled, else false.
      */
-    const wasDetected = (labelYSize, labelXpos) => {
-        // console.log("x:" +labelXpos + " y: " + labelYSize);
-         console.log("pitch:" + panoramaState.pov.pitch);
-         console.log("zoom:" + panoramaState.zoom);
-        //console.log("ratio:");
-        //console.log(labelYSize/window.innerHeight);
-        // Y - axis calculation.
-        // TODO:: need to calculate base on ratio and pitch from db.
-        //const minPitch = -41.696;
-        //const maxPitch = 30.5026;
+    const wasDetected = (e) => {
 
-        console.log("window height:");
-        console.log(window.innerHeight);
+        const [objectXposition, objectYposition] = objectPositionOnScreen(e, panoramaState);
 
-        //Calculate try.
-        let yAxisRatio = 0.6906666666666667;//receives from db.
-        let objectLabeledPitch = 14.746001332508243;//receives from db.
-        let zPitch = "0.6";
-
-        if(panoramaState.zoom > 0.9)
-        {
-            yAxisRatio = 0.76;
-            objectLabeledPitch = 8.912548190430712;
-            zPitch = "1";
-        }
-
-        //750 = window innerHeight of the manager that set the data.
-        const minPitch =  objectLabeledPitch - yAxisRatio*(zoomToPitch[zPitch]);
-        const maxPitch = minPitch + zoomToPitch[zPitch];
-
-        // console.log("max and min pitch:");
-        // console.log(minPitch);
-        // console.log(maxPitch);
-
-        // TODO:: need to retrieve according to panoramaMap location.
-        const windowHeightStart = 0;
-        const windowHeightEnd = window.innerHeight;
-
-        let pitch = panoramaState?.pov?.pitch;
-
-        let ratio = ((Math.abs(minPitch)) + pitch)/(maxPitch - minPitch);
-
-        let objectYposition = (windowHeightEnd - windowHeightStart)*ratio + windowHeightStart;
-
-
-        // X - axis calculation.
-        // TODO:: need to receive from db.
-        let leftHeading = 44;
-        let rightHeading = 300;
-
-        let heading = panoramaState?.pov?.heading;
-
-        let headingInterval = leftHeading - rightHeading;
-        let deg =  heading - rightHeading;
-
-        //Going throw 360deg edge case.
-        if(rightHeading > leftHeading)
-        {
-            headingInterval = (360 - rightHeading) + leftHeading;
-            if(heading < rightHeading) {
-                deg = (360 - rightHeading) + heading;
-            }
-        }
-
-        let xRatio = deg / headingInterval;
-
-        // TODO:: need to retrieve according to panoramaMap location.
-        const windowLeft = 0;
-        const windowRight = window.innerWidth;
-
-        let objectXposition = windowRight - (windowRight - windowLeft) * xRatio + windowLeft;
+        const zPitch = closest(panoramaState.zoom);
+        const xPos = e.clientX;
+        const yPos = e.clientY;
 
         setTrack([objectXposition, objectYposition]);
 
-        //  TODO:: need to change to be according to the label size;
-        return (lables[1]  < objectYposition && lables[1] > objectYposition - 150) &&
-            (labelYSize > objectYposition && labelYSize < objectYposition + 100) &&
-            (objectXposition > 100 + lables[0] && objectXposition + 50 < labelXpos);
+        const size = zoomToRatioData.size;
+
+        const squareStartX =  objectXposition - (size[zPitch].x)/2;
+        const squareEndX =  objectXposition + (size[zPitch].x)/2;
+
+        const squareStartY =  objectYposition - (size[zPitch].y)/2;
+        const squareEndY = objectYposition + (size[zPitch].y)/2;
+
+        const delta = 100;
+        const outSquare =
+            (lables[1]  >= squareStartY - delta && yPos <= squareEndY + delta &&
+             lables[0] >= squareStartX - delta && xPos <= squareEndX + delta);
+
+        const inSquare =
+            (lables[1]  <= squareStartY && yPos >= squareEndY && lables[0] <= squareStartX && xPos >= squareEndX);
+
+        //  TODO:: need to add inner square.
+        return outSquare && inSquare;
     }
 
     const handlePageFinish = (e) => {
         if (mouseDown) {
 
-            // check if props has onLabelSelect
-            if (props.onLabelSelect) {
-                props.onLabelSelect(lables, lablesSize);
-            }
             // flicker animation
-            if(wasDetected(e.clientY, e.clientX) /*&& props.hasFound && props.hasFound(lables, lablesSize)*/)
+            if(wasDetected(e))
             {
                 // player found the object
                 setFlicker('green');
@@ -180,8 +118,24 @@ function LabelSelector(props) {
     // handle ctrl+tab edge case
     return (
         <>
-            <div onMouseDown={(e) => {wasDetected(e.clientY, e.clientX)}}
-                 style={{background: "black", position: "absolute", top: yTrack, left: xTrack, color: "green"}}
+            <div className= "select-none" onMouseDown={(e) => {wasDetected(e)}}
+                 style={{background: "rgba(220,55,55,0.5)", position: "absolute",
+                     top: yTrack - (zoomToRatioData.size[closest(panoramaState.zoom)].y)/2,
+                     left: xTrack - (zoomToRatioData.size[closest(panoramaState.zoom)].x)/2,
+                     width: zoomToRatioData.size[closest(panoramaState.zoom)].x,
+                     height: zoomToRatioData.size[closest(panoramaState.zoom)].y}}
+            >
+            </div>
+            <div className= "select-none" onMouseDown={(e) => {wasDetected(e)}}
+                 style={{background: "rgba(31,71,27,0.5)", position: "absolute",
+                     top: yTrack - (zoomToRatioData.size[closest(panoramaState.zoom)].y)/2 - 40,
+                     left: xTrack - (zoomToRatioData.size[closest(panoramaState.zoom)].x)/2 - 40,
+                     width: zoomToRatioData.size[closest(panoramaState.zoom)].x + 80,
+                     height: zoomToRatioData.size[closest(panoramaState.zoom)].y + 80}}
+            >
+            </div>
+            <div className= "select-none" onMouseDown={(e) => {wasDetected(e)}}
+                 style={{background: "rgba(0,0,0)", position: "absolute", top: yTrack, left: xTrack, color: "green"}}
             >+</div>
 
             <div
---
+++ frontend/src/helpFunctions/LabelSelectorHelpers.js
@@ -0,0 +1,106 @@
+import zoomToRangeData from "../data/ZoomToRange.json";
+import zoomToRatioData from "../data/RatioData.json";
+
+/*
+    TODO:: for zoom > 1, seems like a connection between the zoom to the pitch range ( 1 + zoom = pitch / 2).
+
+    The function receives the current zoom.
+    Returns the zoom with zoom to pitch range function.
+ */
+
+const closest = (currentZoom) => {
+    return (currentZoom > 0.8 && currentZoom < 1.25) ? "1" :
+           (currentZoom < 0.8) ? "0.6" :
+           (currentZoom < 1.8) ? "1.5" : "2";
+}
+
+/*
+    TODO:: move inside of useEffect, with zoom and lat & lng dependencies.
+    For later - console.log(panoramaState?.position.lat() === 37.868996731655);
+
+    The function receives the mouseUp event.
+    Calculating the objects location based on pitch and heading.
+
+    returns true if the object was found, false otherwise.
+ */
+const objectPositionOnScreen = (e, panoramaState) => {
+    // Debugging.
+    // console.log("x:" + e.clientX + " y: " + e.clientY);
+    // console.log("pitch:" + panoramaState.pov.pitch);
+    // console.log("zoom:" + panoramaState.zoom);
+    // console.log("ratio:");
+    // console.log(e.clientX/window.innerWidth);
+    // console.log("heading: " + panoramaState.pov.heading);
+
+    // Y - axis calculation.
+    // TODO:: need to calculate base on ratio and pitch from db.
+
+    let zPitch = closest(panoramaState.zoom);
+    //const xPos = e.clientX;
+    const yPos = e.clientY;
+
+    //Calculate min and max pitch.
+    const zoomToPitchRange = zoomToRangeData.pitch;
+    const zoomToRatioPitch = zoomToRatioData.pitch;
+
+    let yAxisRatio = zoomToRatioPitch[zPitch].ratio;//receives from db.
+    let objectLabeledPitch = zoomToRatioPitch[zPitch].pitch;//receives from db.
+
+    //Object min and max pitch.
+    const minPitch = objectLabeledPitch - yAxisRatio * (zoomToPitchRange[zPitch]);
+    const maxPitch = minPitch + zoomToPitchRange[zPitch];
+
+    // TODO:: need to retrieve according to panoramaMap location.
+    const windowHeightStart = 0;
+    const windowHeightEnd = window.innerHeight;
+
+    let pitch = panoramaState?.pov?.pitch;
+
+    let ratio = ((Math.abs(minPitch)) + pitch) / (maxPitch - minPitch);
+
+    let objectYposition = (windowHeightEnd - windowHeightStart) * ratio + windowHeightStart
+        - 25 * yPos / window.innerHeight;
+
+
+    // X - axis calculation.
+    // TODO:: need to receive from db.
+
+    //Calculate min and max heading.
+    const zoomToRatioHeading = zoomToRatioData.heading;
+    const zoomToHeadingRange = zoomToRangeData.heading;
+
+    let xAxisRatio = zoomToRatioHeading[zPitch].ratio;//receives from db.
+    let objectLabeledHeading = zoomToRatioHeading[zPitch].heading;//receives from db.
+
+    let maxHeading = (objectLabeledHeading + (xAxisRatio * zoomToHeadingRange[zPitch])) % 360;
+    let minHeading = (objectLabeledHeading - (1 - xAxisRatio) * zoomToHeadingRange[zPitch]);
+
+    if (minHeading < 0) {
+        minHeading += 360;
+    }
+
+    let heading = panoramaState?.pov?.heading;
+
+    let headingInterval = maxHeading - minHeading;
+    let deg = heading - minHeading;
+
+    //Going throw 360deg edge case.
+    if (minHeading > maxHeading) {
+        headingInterval = (360 - minHeading) + maxHeading;
+        if (heading < minHeading) {
+            deg = (360 - minHeading) + heading;
+        }
+    }
+
+    let xRatio = deg / headingInterval;
+
+    const windowLeft = 0;
+    const windowRight = window.innerWidth;
+
+    let objectXposition = windowRight - (windowRight - windowLeft) * xRatio + windowLeft;
+
+    return [objectXposition, objectYposition];
+}
+
+
+export { objectPositionOnScreen, closest };

3f468a5a16d6517f3aab92fbc5a59be66482fdde
ebercovich <102984490+ebercovich@users.noreply.github.com>
2023-11-12 00:04:10 +0200

    correction

--- frontend/src/components/homePage/Body.js
+++ frontend/src/components/homePage/Body.js
@@ -3,18 +3,19 @@ import IdTicket from "./idTicket.svg";
 export default function Body() {
   return (
     <div className="flex flex-col h-full ">
-      <div className="grid grid-cols-4 grid-rows-2">
-        <div className="col-end-5"/>
-        <div/>
+      <div className="grid grid-cols-10 grid-rows-2">
+
         <Card
           text="Yellow Car"
           img={YCar}
           description="Find as many as you can!"
+          css="col-start-4 col-end-6 row-start-2"
         />
         <Card
           text="Intelligence"
           img={IdTicket}
           description="Find the object!"
+          css="col-start-6 col-end-8 row-start-2"
         />
       </div>
     </div>
@@ -23,7 +24,9 @@ export default function Body() {
 
 function Card(props) {
   return (
-    <div className="flex flex-col m-4 p-10 rounded-3xl bg-slate-800">
+    <div
+      className={"flex flex-col m-4 p-10 rounded-3xl bg-slate-800 " + props.css}
+    >
       <div className="flex flex-col">
         <h1 className="text-white text-2xl">{props.text}</h1>
         <p className="text-green-200">{props.description}</p>
